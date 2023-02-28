7c38d0a692b22ebe94aede66dbf9aaeb640bbd96
druckhead <daniel32raz@gmail.com>
2023-02-28 14:09:28 +0200

    add missing Meta class

--- jwt2_flights/backend/api/flights/serializers.py
+++ jwt2_flights/backend/api/flights/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 
 from .models import Flight
 
+
 class FlightSerializer(serializers.ModelSerializer):
-    model = Flight
-    fields = "__all__"
+    class Meta:
+        model = Flight
+        fields = "__all__"
