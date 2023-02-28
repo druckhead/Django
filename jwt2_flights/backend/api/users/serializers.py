e52e25894c5c70fa01785f123222ccc888040913
druckhead <daniel32raz@gmail.com>
2023-02-28 15:07:03 +0200

    create UserRegistrationSerializer

--- jwt2_flights/backend/api/users/serializers.py
+++ jwt2_flights/backend/api/users/serializers.py
@@ -5,6 +5,56 @@ from rest_framework.exceptions import ValidationError
 from rest_framework.validators import UniqueValidator
 
 
+class UserResgisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "first_name",
+            "last_name",
+            "email",
+            "username",
+            "password",
+            "confirm_password",
+        ]
+        extra_kwargs = {
+            "first_name": {
+                "required": True,
+            },
+            "last_name": {
+                "required": True,
+            },
+            "email": {
+                "required": True,
+                "validators": [
+                    UniqueValidator(queryset=User.objects.all()),
+                ],
+            },
+            "password": {"write_only": True, "required": True},
+            "password_confirm": {"write_only": True, "required": True},
+        }
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["confirm_password"]:
+            raise ValidationError({"password": "Password Fields must match."})
+        try:
+            validate_password(attrs["password"])
+        except ValidationError as validerr:
+            raise serializers.ValidationError({"password": validerr})
+
+        return attrs
+
+    def create(self, validated_data):
+        user = User(
+            first_name=validated_data["first_name"],
+            last_name=validated_data["last_name"],
+            email=validated_data["email"],
+        )
+        user.set_password(validated_data["password"])
+        user.save()
+
+        return user
+
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
