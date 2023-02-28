d6982a6f9502d73fb48767e4574d930d0614d195
druckhead <daniel32raz@gmail.com>
2023-02-28 23:44:42 +0200

    delete

--- jwt_2_flights/backend/airline_app/flights/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import Flight
-
-
-class FlightSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Flight
-        fields = ()
--- jwt_2_flights/backend/airline_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import include, path
-
-
-urlpatterns = [
-    path("flights/", include("airline_app.flights.urls")),
-    path("orders/", include("airline_app.orders.urls")),
-    path("users/", include("airline_app.users.urls")),
-]
