5ce33ef27be4877571d902b4759a7474d691c26c
druckhead <daniel32raz@gmail.com>
2023-03-01 02:31:30 +0200

    add imports

--- jwt2_flights/backend/api/views.py
+++ jwt2_flights/backend/api/views.py
@@ -1,3 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
+from .flights.views import FlightViewSet
+from .orders.views import OrderViewSet
+from .users.views import UserViewSet
