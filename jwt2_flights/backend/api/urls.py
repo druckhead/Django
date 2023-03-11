56a5db5b99e9580ad9f17b6e4edad2d03c0f3bae
druckhead <daniel32raz@gmail.com>
2023-03-11 13:41:39 +0200

    add registeration to url router path

--- jwt2_flights/backend/api/urls.py
+++ jwt2_flights/backend/api/urls.py
@@ -6,16 +6,17 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-from .views import (
-    FlightViewSet,
-    OrderViewSet,
-    UserViewSet,
-)
+from .users.views import StaffRegistrationViewSet
+
+from .views import FlightViewSet, OrderViewSet, UserViewSet, UserRegistrationViewSet
 
 router = DefaultRouter()
 router.register(r"flights", FlightViewSet, basename="flights")
 router.register(r"orders", OrderViewSet, basename="orders")
 router.register(r"users", UserViewSet, basename="users")
+router.register(r"register", UserRegistrationViewSet, basename="register")
+router.register(r"register/staff", StaffRegistrationViewSet, basename="register_staff")
+
 
 urlpatterns = [
     path("auth/token/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
